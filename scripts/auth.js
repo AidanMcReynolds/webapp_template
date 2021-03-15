45ae6f4e5a581189bc9fbcff8cc467fdf3686e07
AidanMcReynolds <akmcr@yahoo.ca>
2021-03-15 15:24:48 -0700

    updated auth

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,13 +1,3 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyB2qPnJ_euWzWG-9BC2qqm8Zm5csulv3rY",
-  authDomain: "march-a62dc.firebaseapp.com",
-  projectId: "march-a62dc",
-  storageBucket: "march-a62dc.appspot.com",
-  messagingSenderId: "727267053387",
-  appId: "1:727267053387:web:d7bdcf5c7274fef16a62ce",
-  measurementId: "G-RGGPM4X6C3"
-};
-firebase.initializeApp(firebaseConfig);
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     console.log("hello " + user.email)
---
+++ scripts/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyB2qPnJ_euWzWG-9BC2qqm8Zm5csulv3rY",
+  authDomain: "march-a62dc.firebaseapp.com",
+  projectId: "march-a62dc",
+  storageBucket: "march-a62dc.appspot.com",
+  messagingSenderId: "727267053387",
+  appId: "1:727267053387:web:d7bdcf5c7274fef16a62ce",
+  measurementId: "G-RGGPM4X6C3"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
