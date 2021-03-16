ea46656eaff5c2e4d1118d1a3ca0e374545b9d9d
AidanMcReynolds <akmcr@yahoo.ca>
2021-03-16 15:05:40 -0700

    main.html updated such that pages now refect task list in database

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,16 +1,25 @@
-var user;
-firebase.auth().onAuthStateChanged(function (u) {
-  user = u;
+
+firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     console.log("hello " + user.email)
   } else {
     window.location.pathname = "/index.html"
   }
-  db.collection("users").doc(user.uid).get().then(function(ut) {
-    if (ut.exists) {
-      console.log("hello " + ut.data().name);
+ 
+});
+firebase.auth().onAuthStateChanged(function (user) {
+  db.collection("users").doc(user.uid).get().then(function(u) {
+    if (u.exists) {
+      console.log("hello " + u.data().name);
     }
-  
   });
-  
-})
+});
+
+window.addEventListener("load", () => {
+  var user = firebase.auth().currentUser;
+  if (user != null){
+    console.log(user.uid);
+  } else {
+    console.log("blah");
+  }
+});
