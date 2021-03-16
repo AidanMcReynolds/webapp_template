58269ad985046c868ea1f7039369ed2882c08c8d
AidanMcReynolds <akmcr@yahoo.ca>
2021-03-16 16:09:56 -0700

    updated tasks.js

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,26 +1,26 @@
 firebase.auth().onAuthStateChanged(function (user) {
   db.collection("users").doc(user.uid).collection("tasks").where("deleted", "!=", true).get().then((tasks) => {
-    console.log(tasks.size);
-    tasks.forEach((t) => {
-      console.log(t.data().name);
-    });
-    // update the size of the container for the task list
-    cont = document.getElementById("task-container");
-    let rows = ""
-    for(i=0;i<=tasks.size;i++){
-      rows = rows + "15vh "
-    }
-    cont.style.gridTemplateRows = rows;
-    //append the table with the user's tasks
-    tasks.forEach((t) =>{
-      cont.innerHTML = cont.innerHTML + taskRow(t.data().name);
-    });
-    //append the table with the add task button
-    cont.innerHTML = cont.innerHTML + taskAdd();
-    feather.replace()
-    cont.style.visibility = "visible";
+    taskTable(tasks);
   });
 });
+function taskTable(tasks){
+  // update the size of the container for the task list
+  cont = document.getElementById("task-container");
+  cont.innerHTML = "";
+  let rows = ""
+  for(i=0;i<=tasks.size;i++){
+    rows = rows + "40pt "
+  }
+  cont.style.gridTemplateRows = rows;
+  //append the table with the user's tasks
+  tasks.forEach((t) =>{
+    cont.innerHTML = cont.innerHTML + taskRow(t.data().name);
+  });
+  //append the table with the add task button
+  cont.innerHTML = cont.innerHTML + taskAdd();
+  feather.replace()
+  cont.style.visibility = "visible";
+}
 function taskRow(taskName){
   let r = "";
   r = '<div class="task-check"><input class="form-check-input me-1" type="checkbox" value="" aria-label="..."></div>';
