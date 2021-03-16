786ccb6ffa76a990e6e22b99d99d2c913841c750
AidanMcReynolds <akmcr@yahoo.ca>
2021-03-15 17:21:38 -0700

    updated db

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,7 +1,16 @@
-firebase.auth().onAuthStateChanged(function (user) {
+var user;
+firebase.auth().onAuthStateChanged(function (u) {
+  user = u;
   if (user) {
     console.log("hello " + user.email)
   } else {
     window.location.pathname = "/index.html"
   }
-});
+  db.collection("users").doc(user.uid).get().then(function(ut) {
+    if (ut.exists) {
+      console.log("hello " + ut.data().name);
+    }
+  
+  });
+  
+})
