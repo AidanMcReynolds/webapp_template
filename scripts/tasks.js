4833a51bec58ace8b90ed3b8ac437e55cc6550b3
AidanMcReynolds <akmcr@yahoo.ca>
2021-03-22 19:29:27 -0700

    split the onAuthStateChanged function

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,4 +1,7 @@
 firebase.auth().onAuthStateChanged(function (user) {
+  taskUpdate(user);
+});
+function taskUpdate(user){
   db.collection("users").doc(user.uid).collection("tasks").where("deleted", "!=", true).get().then((tasks) => {
     console.log(tasks.size);
     tasks.forEach((t) => {
@@ -7,12 +10,12 @@ firebase.auth().onAuthStateChanged(function (user) {
     // update the size of the container for the task list
     cont = document.getElementById("task-container");
     let rows = ""
-    for(i=0;i<=tasks.size;i++){
+    for (i = 0; i <= tasks.size; i++) {
       rows = rows + "15vh "
     }
     cont.style.gridTemplateRows = rows;
     //append the table with the user's tasks
-    tasks.forEach((t) =>{
+    tasks.forEach((t) => {
       cont.innerHTML = cont.innerHTML + taskRow(t.data().name);
     });
     //append the table with the add task button
@@ -24,21 +27,21 @@ firebase.auth().onAuthStateChanged(function (user) {
     document.getElementById("exampleModalTask").addEventListener("submit", submitTaskDB);
     taskTable(tasks);
   });
-});
-function taskTable(tasks){
+}
+function taskTable(tasks) {
   // update the size of the container for the task list
   cont = document.getElementById("task-container");
   cont.innerHTML = "";
   let rows = ""
-  for(i=0;i<=tasks.size;i++){
+  for (i = 0; i <= tasks.size; i++) {
     rows = rows + "40pt "
   }
   cont.style.gridTemplateRows = rows;
   //append the table with the user's tasks
-  tasks.forEach((t) =>{
-    cont.innerHTML = cont.innerHTML + taskRow(t.data().name,t.id);
+  tasks.forEach((t) => {
+    cont.innerHTML = cont.innerHTML + taskRow(t.data().name, t.id);
   });
-  tasks.forEach((t) =>{
+  tasks.forEach((t) => {
     checkboxUpdate(t);
   });
   //append the table with the add task button
@@ -46,18 +49,18 @@ function taskTable(tasks){
   feather.replace()
   cont.style.visibility = "visible";
 }
-function taskRow(taskName, taskID){
+function taskRow(taskName, taskID) {
   let r = "";
   r = '<div class="task-check"><input class="form-check-input me-1" onclick="taskClick(this)" type="checkbox" id="check_' + taskID + '" value="' + taskID + '" aria-label="..."></div>';
   r = r + '<div class="task-text">' + taskName + '</div>';
   r = r + '<div class="task-del"><button type="button" class="btn btn-outline-danger btn-sm"><i data-feather="trash-2"></i></button></div>'
   return r;
 }
-function taskAdd(){
+function taskAdd() {
   return '<div class="task-add"><button type="button" class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModalTask"><i data-feather="plus"></i></button></div>'
 }
 //------Michael------------------------------->
-function submitTaskDB(e){
+function submitTaskDB(e) {
   e.preventDefault();
 
   var task = getInputValue("task");
@@ -66,10 +69,10 @@ function submitTaskDB(e){
 
   saveTask(task);
 }
-function getInputValue(id){
+function getInputValue(id) {
   return document.getElementById(id).value;
 }
-function saveTask(name){
+function saveTask(name) {
   var taskRef = db.collection("users").doc(firebase.auth().currentUser.uid).collection("tasks");
 
   taskRef.add({
@@ -79,24 +82,24 @@ function saveTask(name){
   });
 }
 
-function displayTasks(){
+function displayTasks() {
   db.collection("users").doc(firebase.auth().currentUser.uid).collection("tasks").get()
-  .then(function(snap){
-      snap.forEach(function(doc){
-          var n = doc.data().name;
-          console.log(n);
-          var delet = doc.data().deleted;
-          console.log(delet);
-          var createdTime = doc.data().created;
-          console.log(createdTime);
-          // document.getElementById(cityId).textContent = n;
+    .then(function (snap) {
+      snap.forEach(function (doc) {
+        var n = doc.data().name;
+        console.log(n);
+        var delet = doc.data().deleted;
+        console.log(delet);
+        var createdTime = doc.data().created;
+        console.log(createdTime);
+        // document.getElementById(cityId).textContent = n;
       })
-  })
+    })
 }
 displayTasks();
 //---Michael ------->
-function taskClick(id){
-  if (id.checked){
+function taskClick(id) {
+  if (id.checked) {
     console.log("checked");
     taskDo(id.value);
   } else {
@@ -104,13 +107,13 @@ function taskClick(id){
     taskUndo(id.value);
   }
 }
-function taskDo(id){
+function taskDo(id) {
   today = firebase.firestore.Timestamp.now();
   user = firebase.auth().currentUser;
   db.collection("users").doc(user.uid).collection("tasks").doc(id).get().then((task) => {
     dates = task.data().completed;
-    for(i=0;i<dates.length;i++){
-      if (today.toDate().toDateString()==dates[i].toDate().toDateString()){
+    for (i = 0; i < dates.length; i++) {
+      if (today.toDate().toDateString() == dates[i].toDate().toDateString()) {
         return;
       }
     }
@@ -120,13 +123,13 @@ function taskDo(id){
   });
 
 }
-function taskUndo(id){
+function taskUndo(id) {
   today = firebase.firestore.Timestamp.now();
   user = firebase.auth().currentUser;
   db.collection("users").doc(user.uid).collection("tasks").doc(id).get().then((task) => {
     dates = task.data().completed;
-    for(i=0;i<dates.length;i++){
-      if (today.toDate().toDateString()==dates[i].toDate().toDateString()){
+    for (i = 0; i < dates.length; i++) {
+      if (today.toDate().toDateString() == dates[i].toDate().toDateString()) {
         db.collection("users").doc(user.uid).collection("tasks").doc(id).update({
           completed: firebase.firestore.FieldValue.arrayRemove(dates[i])
         });
@@ -137,11 +140,11 @@ function taskUndo(id){
 function checkboxUpdate(task) {
   today = firebase.firestore.Timestamp.now();
   dates = task.data().completed;
-  if (dates != null){
+  if (dates != null) {
     for (i = 0; i < dates.length; i++) {
       if (today.toDate().toDateString() == dates[i].toDate().toDateString()) {
         checkbox = document.getElementById("check_" + task.id);
-        checkbox.setAttribute("checked",true);
+        checkbox.setAttribute("checked", true);
         console.log(checkbox);
       }
     }
