dd6a39040eefe376f821bdb74e39f34297ea3a1d
syakisoba <syakisoba@gmail.com>
2021-03-28 13:58:20 -0700

    name, email connected to DB

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,3 +1,4 @@
+
 //popover enabled
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
@@ -17,4 +18,55 @@ function previewFile() {
   if (file) {
     reader.readAsDataURL(file);
   }
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+  console.log(user.photoURL);
+  // user.photoUrl = "./images/profile_random.jpeg";
+  // document.getElementById("previewImag").innerHTML = user.photoUrl;
+  setInputValue("displayNameText", "<h5>" + user.displayName + "</h5>");
+  setInputValue("emailText", "<h5>" + user.email + "</h5>");
+
+  document.getElementById("modalInputName").setAttribute("value", user.displayName);
+  document.getElementById("modalInputEmail").setAttribute("value", user.email);
+
+  document.getElementById("exampleModalEmail").addEventListener("submit", submitTaskDB);
+  feather.replace()
+});
+
+function submitTaskDB(e) {
+  e.preventDefault();
+
+  var user = firebase.auth().currentUser;
+  var newEmail = getInputValue("task");
+  //console.log(task);
+  //console.log(new Date().getTime());
+
+  changeEmail(newEmail);
+  $('#exampleModalEmail').modal('hide');
+  document.getElementById("form-task").reset();
+  taskUpdate(user);
+}
+
+function setInputValue(name, content){
+  document.getElementById(name).innerHTML = content;
+}
+
+function getInputValue(id) {
+  return document.getElementById(id).value;
+}
+function changeEmail(name) {
+
+  firebase.auth().currentUser.updateEmail(name).then(function () {
+    // Update successful.
+    console.log(name);
+    db.collection("users").doc(user.uid).update({         //write to firestore                 //"users" collection
+      email: user.email                          //with authenticated user's ID (user.uid)
+    }).then(function () {
+      console.log("changed user email");
+      window.location.assign("profile.html");       //re-direct to main.html after signup
+    }).catch(function (error) {
+      console.log("Error adding new user: " + error);
+    });
+  });
 }
