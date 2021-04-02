ca67f2342b9794a110e840af8dc48496e5e9b491
AidanMcReynolds <akmcr@yahoo.ca>
2021-04-02 16:52:55 -0700

    fixed formatting in code.

--- scripts/datepicker.js
+++ scripts/datepicker.js
@@ -11,19 +11,21 @@ $(function () {
     });
 });
 
+//this lets tasks.js know user is on datepicker page
 $(document).ready(function () {
-    //$("#datepicker").datepicker("show");
     datepicker = true;
 });
 
+//this changes the date to the selected date. 
 function changeDate(date) {
-  //console.log(date);
   date.setHours(1);
   today = date;
   updateDateBlock()
   let user = firebase.auth().currentUser;
   taskUpdate(user);
 }
+
+//display the selected date
 function updateDateBlock(){
   $("#date-day").html(today.toLocaleString('en-us', {  weekday: 'long' }));
   $("#date-date").html(today.getDate());
@@ -31,4 +33,5 @@ function updateDateBlock(){
   $("#date-year").html(today.toLocaleString('en-us', {  year: 'numeric' }));
 }
 
+//call updateDateblock so that the date block displays the current date when user loads the page
 updateDateBlock();
