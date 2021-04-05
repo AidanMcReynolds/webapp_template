c5c52a64b59cf83ef94c2613d12402af7a759de1
syakisoba <syakisoba@gmail.com>
2021-04-05 14:05:15 -0700

    weekly progress

--- scripts/progress.js
+++ scripts/progress.js
@@ -6,9 +6,13 @@ firebase.auth().onAuthStateChanged(function (user) {
 //  Need all of the tasks data from the user - user collection -> task collection (reading database)
 function progressUpdate(user) {
     db.collection("users").doc(user.uid).collection("tasks").where("deleted", "==", false).get().then((tasks) => {
-        progressDisplay(progressPercent(tasks));
+        progressDisplay(progressPercent(tasks), "progress1");
         console.log(tasks)
         console.log(progressPercent(tasks));
+
+        progressDisplay(progressPercentWeek(tasks), "progress2");
+        console.log(tasks)
+        console.log(progressPercentWeek(tasks));
     });
 }
 
@@ -23,7 +27,7 @@ function progressPercent(tasks) {
     return n/days;
 }
 
-//returns number of days since the task was completed
+//returns number of days since the task was created
 function progressDays(task) {
     var d = firebase.firestore.Timestamp.now()
     var c = task.data().created;
@@ -35,7 +39,29 @@ function progressDays(task) {
 }
 
 //updates the progress bar
-function progressDisplay(variable) {
-    let x = document.getElementById("progress1");
+function progressDisplay(variable , eleID) {
+    let x = document.getElementById(eleID);
     x.style.width = " " + (variable * 100) + "%";
+}
+
+//returns sum of all times all tasks within last week (7 days) completed divided by the total number of times tasks could have been completed
+function progressPercentWeek(task){
+    let days = 0;
+    let n = 0;
+    task.forEach((t) => {
+        let m = 0;
+        t.data().completed.forEach((s) => {
+            if(s > firebase.firestore.Timestamp.now()-(60*60*24*7)){
+                m++;
+            }
+        });
+        let x = progressDays(t);
+        if(x > 7){
+            days = days + 7;
+        }else{
+            days = days + x;
+        }
+        n = n + m;
+    });
+    return n/days;
 }
